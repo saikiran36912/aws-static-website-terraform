f5731a7f829b4ecb1ef12f1a48af66dc7f7a2552
saikiran36912 <144526262+saikiran36912@users.noreply.github.com>
2026-06-12 12:28:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "website" {
-  bucket = "psk-terraform-demo-123456"
+  bucket = var.bucket_name
 }
 
 resource "aws_s3_bucket_public_access_block" "block" {
